6dcce5de05834ee4af4f507eff1026aa7f6863fd
khengschew <khengc@gmail.com>
2019-01-02 11:52:11 -0800

    Implement stringifyJSON primitive handling

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // BASE CASE 
+  
+  // if typof of 'obj' is 'string' or 'boolean' 
+  // return '"' + obj + '"'
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  // of 'obj' === 'null'
+  // return 'null'
+  if (obj === null) {
+    return 'null';
+  }
+
+  // Primitives
+  // Numbers
+  // Strings
+  // Booleans
+  // Undefined
+  // Null 
+  // Special (NaN, -Infinity, Infinity)
+
+  // RECURSIVE CASE 
+  // Objects
+  // Arrays
+  // Nesting
 };
