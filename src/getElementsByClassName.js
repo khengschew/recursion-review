978dfddd0e40aa7784ddd1a1cdd66e2d86898fdc
khengschew <khengc@gmail.com>
2019-01-02 16:27:37 -0800

    Semi-implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, elements) {
   // your code here
+  var tempArr = [];
+  // var docBody = document.body;
+
+  // // Inner function
+  // var recurseElements = function(element) {
+    
+  // };
+
+  // tempElement = [document.body] || elements;
+  var tempElement = elements || [document.body];
+
+  // BASE CASE
+  // If element is an empty array, return empty array
+  if (tempElement.length <= 0) {
+    return [];
+  }
+
+  // If tempElement.classList contains className
+  if (tempElement[0].classList && tempElement[0].classList.contains(className)) {
+    tempArr.push(tempElement[0]);
+  }
+
+  // If element has no children, and 'tempElement' is of length 1
+  if (tempElement.length <= 1 && tempElement[0].childNodes.length === 0) {
+    return tempArr;
+  }
+
+  // RECURSIVE CASE
+  // Push to temp array
+  // return temp array concatenate recursive results from children, and other elements;
+  return tempArr
+    .concat(getElementsByClassName(className, tempElement[0].childNodes))
+    .concat(getElementsByClassName(className, tempElement.slice(1)));
+
+  // return tempArr;
 };
